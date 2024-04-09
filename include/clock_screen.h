470394dffb9a1eaeb6a2caa1a49d8dca6e7a6422
j1nxie <rylieeeeexd@gmail.com>
2024-04-09 17:34:57 +0700

    feat: clean slate

--- include/clock_screen.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLOCK_SCREEN_H
-#define CLOCK_SCREEN_H
-
-#include "app_state.h"
-
-void ClockScreen(AppState *app_state);
-
-#endif
--- src/app_state.c
+++
@@ -1,5 +0,0 @@
-#include "../include/app_state.h"
-
-AppState initialize_app(int minutes, int seconds, int pomodoros) {
-    return (AppState){minutes, seconds, pomodoros};
-}
