e4ea3b429238832699e4bc9bc2c0099c90b4fe11
j1nxie <rylieeeeexd@gmail.com>
2024-04-14 18:23:28 +0700

    build: switch to using nix flake

--- shell.nix
+++
@@ -1,6 +0,0 @@
-{pkgs ? import <nixpkgs> {}}:
-
-pkgs.mkShell {
-  packages = with pkgs; [ zig zls ];
-  buildInputs = with pkgs; [ libGL xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXinerama xorg.libXi ];
-}
