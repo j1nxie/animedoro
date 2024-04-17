9f25fe34eba1c59ab6130ce841af6dbfd7986060
j1nxie <rylieeeeexd@gmail.com>
2024-04-17 13:25:32 +0700

    fix: what was i doing

--- src/clock_screen.c
+++ src/clock_screen.c
@@ -2,30 +2,7 @@
 #include "../include/app_state.h"
 #include <raylib.h>
 
-void CheckState(AppState *app_state, AppConfig *app_config) {
-    switch (app_state->state) {
-    case (POMODORO_CLOCK):
-        app_state->minutes = app_config->pomodoro_length;
-        if (app_state->pomodoros > 0) {
-            app_state->pomodoros--;
-            app_state->state = POMODORO_SHORT_BREAK;
-        } else {
-            app_state->pomodoros = 4;
-            app_state->state = POMODORO_LONG_BREAK;
-        }
-        break;
-    case (POMODORO_SHORT_BREAK):
-        app_state->minutes = app_config->short_break_length;
-        app_state->state = POMODORO_CLOCK;
-        break;
-    case (POMODORO_LONG_BREAK):
-        app_state->minutes = app_config->long_break_length;
-        app_state->state = POMODORO_CLOCK;
-        break;
-    }
-}
-
-void SkipState(AppState *app_state, AppConfig *app_config) {
+void NextState(AppState *app_state, AppConfig *app_config) {
     switch (app_state->state) {
     case (POMODORO_CLOCK):
         if (app_state->pomodoros > 0) {
@@ -72,7 +49,7 @@ void ClockScreen(AppState *app_state, AppConfig *app_config) {
         }
 
         if (app_state->minutes == 0) {
-            CheckState(app_state, app_config);
+            NextState(app_state, app_config);
             app_state->running = false;
         }
         if (GuiButton((Rectangle){GetScreenWidth() / 2.0f - 45.0f,
@@ -98,7 +75,7 @@ void ClockScreen(AppState *app_state, AppConfig *app_config) {
     if (GuiButton((Rectangle){GetScreenWidth() / 2.0f + 7.0f,
                               GetScreenHeight() / 2.0f + 45.0f, 32, 32},
                   GuiIconText(ICON_PLAYER_NEXT, ""))) {
-        SkipState(app_state, app_config);
+        NextState(app_state, app_config);
         app_state->running = false;
     }
 }
