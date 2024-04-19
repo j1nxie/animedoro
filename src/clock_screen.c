81571f5f4d5f7c2c6becc75e6a97c4f033c51c8b
j1nxie <rylieeeeexd@gmail.com>
2024-04-20 01:20:33 +0700

    style: indent a few more things

--- src/clock_screen.c
+++ src/clock_screen.c
@@ -4,22 +4,22 @@
 
 void NextState(AppState *app_state, AppConfig *app_config) {
     switch (app_state->state) {
-    case (POMODORO_CLOCK):
-        if (app_state->pomodoros > 0) {
-            app_state->pomodoros--;
-            app_state->state = POMODORO_SHORT_BREAK;
-            app_state->minutes = app_config->short_break_length;
-        } else {
-            app_state->pomodoros = 3;
-            app_state->state = POMODORO_LONG_BREAK;
-            app_state->minutes = app_config->long_break_length;
-        }
-        break;
-    case (POMODORO_SHORT_BREAK):
-    case (POMODORO_LONG_BREAK):
-        app_state->state = POMODORO_CLOCK;
-        app_state->minutes = app_config->pomodoro_length;
-        break;
+        case (POMODORO_CLOCK):
+            if (app_state->pomodoros > 0) {
+                app_state->pomodoros--;
+                app_state->state = POMODORO_SHORT_BREAK;
+                app_state->minutes = app_config->short_break_length;
+            } else {
+                app_state->pomodoros = 3;
+                app_state->state = POMODORO_LONG_BREAK;
+                app_state->minutes = app_config->long_break_length;
+            }
+            break;
+        case (POMODORO_SHORT_BREAK):
+        case (POMODORO_LONG_BREAK):
+            app_state->state = POMODORO_CLOCK;
+            app_state->minutes = app_config->pomodoro_length;
+            break;
     }
     app_state->seconds = 0.0f;
 }
@@ -27,32 +27,33 @@ void NextState(AppState *app_state, AppConfig *app_config) {
 void ClockScreen(AppState *app_state, AppConfig *app_config) {
     float time = app_state->minutes * 60 + app_state->seconds;
     switch (app_state->state) {
-    case (POMODORO_CLOCK):
-        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(),
-                      CLITERAL(Color){246, 126, 125, 255});
-        DrawRectangle(0, 0,
-                      GetScreenWidth() *
-                          (1.0f - (time / (app_config->pomodoro_length * 60))),
-                      GetScreenHeight(), CLITERAL(Color){179, 255, 179, 255});
-        break;
-    case (POMODORO_SHORT_BREAK):
-        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(),
-                      CLITERAL(Color){179, 255, 179, 255});
-        DrawRectangle(
-            0, 0,
-            GetScreenWidth() *
-                (1.0f - (time / (app_config->short_break_length * 60))),
-            GetScreenHeight(), CLITERAL(Color){246, 126, 125, 255});
-        break;
-    case (POMODORO_LONG_BREAK):
-        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(),
-                      CLITERAL(Color){179, 255, 179, 255});
-        DrawRectangle(
-            0, 0,
-            GetScreenWidth() *
-                (1.0f - (time / (app_config->long_break_length * 60))),
-            GetScreenHeight(), CLITERAL(Color){246, 126, 125, 255});
-        break;
+        case (POMODORO_CLOCK):
+            DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(),
+                          CLITERAL(Color){246, 126, 125, 255});
+            DrawRectangle(
+                0, 0,
+                GetScreenWidth() *
+                    (1.0f - (time / (app_config->pomodoro_length * 60))),
+                GetScreenHeight(), CLITERAL(Color){179, 255, 179, 255});
+            break;
+        case (POMODORO_SHORT_BREAK):
+            DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(),
+                          CLITERAL(Color){179, 255, 179, 255});
+            DrawRectangle(
+                0, 0,
+                GetScreenWidth() *
+                    (1.0f - (time / (app_config->short_break_length * 60))),
+                GetScreenHeight(), CLITERAL(Color){246, 126, 125, 255});
+            break;
+        case (POMODORO_LONG_BREAK):
+            DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(),
+                          CLITERAL(Color){179, 255, 179, 255});
+            DrawRectangle(
+                0, 0,
+                GetScreenWidth() *
+                    (1.0f - (time / (app_config->long_break_length * 60))),
+                GetScreenHeight(), CLITERAL(Color){246, 126, 125, 255});
+            break;
     }
     if (IsKeyPressed(KEY_SPACE)) {
         app_state->running = !app_state->running;
@@ -103,7 +104,7 @@ void ClockScreen(AppState *app_state, AppConfig *app_config) {
             TextFormat("clock paused >:c - time remaining: %s", string));
     }
 
-    DrawText(string, (int)text_pos.x, (int)text_pos.y, 40, BLUE);
+    DrawText(string, (int)text_pos.x, (int)text_pos.y, 40, WHITE);
     if (GuiButton((Rectangle){GetScreenWidth() / 2.0f,
                               GetScreenHeight() / 2.0f + 45.0f - 16.0f, 32, 32},
                   GuiIconText(ICON_PLAYER_NEXT, ""))) {
