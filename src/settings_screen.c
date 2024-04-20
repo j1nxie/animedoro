52ed8aef2b1c2c256564997751ab62fd354bd831
j1nxie <rylieeeeexd@gmail.com>
2024-04-20 11:26:20 +0700

    feat: the rest of the settings screen

--- src/settings_screen.c
+++ src/settings_screen.c
@@ -1,6 +1,19 @@
 #include "../deps/raygui.h"
 #include "../include/app_state.h"
 #include <raylib.h>
+#include <stdint.h>
+
+const int LINE_PADDING = 8;
+
+// i am truly sorry to all who dislikes global states.
+bool pomodoro_edit = false;
+int *pomodoro_edit_value = &app_config.pomodoro_length;
+
+bool short_break_edit = false;
+int *short_break_edit_value = &app_config.short_break_length;
+
+bool long_break_edit = false;
+int *long_break_edit_value = &app_config.long_break_length;
 
 void SettingsScreen(AppState *app_state, AppConfig *app_config) {
     DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), RAYWHITE);
@@ -9,5 +22,30 @@ void SettingsScreen(AppState *app_state, AppConfig *app_config) {
         app_state->current_screen = CLOCK_SCREEN;
     }
 
-    // TODO: draw the rest of the screen
+    GuiSetStyle(VALUEBOX, TEXT_PADDING, 4);
+    GuiSetStyle(VALUEBOX, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
+    GuiSetStyle(CHECKBOX, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
+
+    if (GuiValueBox((Rectangle){32, 32, 32, 32}, "pomodoro length",
+                    pomodoro_edit_value, 1, INT32_MAX, pomodoro_edit)) {
+        app_config->pomodoro_length = *pomodoro_edit_value;
+        pomodoro_edit = !pomodoro_edit;
+    }
+
+    if (GuiValueBox((Rectangle){32, 32 * 2 + LINE_PADDING, 32, 32},
+                    "short break length", short_break_edit_value, 1, INT32_MAX,
+                    short_break_edit)) {
+        app_config->short_break_length = *short_break_edit_value;
+        short_break_edit = !short_break_edit;
+    }
+
+    if (GuiValueBox((Rectangle){32, 32 * 3 + 2 * LINE_PADDING, 32, 32},
+                    "long break length", long_break_edit_value, 1, INT32_MAX,
+                    long_break_edit)) {
+        app_config->long_break_length = *long_break_edit_value;
+        long_break_edit = !long_break_edit;
+    }
+
+    GuiCheckBox((Rectangle){32, 32 * 4 + 3 * LINE_PADDING, 32, 32},
+                "automatically start timer", &app_config->auto_start_timer);
 }
