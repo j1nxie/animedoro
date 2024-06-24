886c911b02af90420000b136b43c5119c8fcfce5
j1nxie <rylieeeeexd@gmail.com>
2024-06-24 17:44:24 +0700

    fix: set default config/state to 45 / 25 / 25

--- src/app_state.c
+++ src/app_state.c
@@ -5,5 +5,5 @@ AppState initialize_app(AppConfig *app_config) {
         app_config->pomodoro_length, 0, 3, false, POMODORO_CLOCK, CLOCK_SCREEN};
 }
 
-AppConfig app_config = (AppConfig){45, 5, 15, false};
-AppState app_state = (AppState){25, 0, 3, false, POMODORO_CLOCK, CLOCK_SCREEN};
+AppConfig app_config = (AppConfig){45, 25, 25, false};
+AppState app_state = (AppState){45, 0, 3, false, POMODORO_CLOCK, CLOCK_SCREEN};
